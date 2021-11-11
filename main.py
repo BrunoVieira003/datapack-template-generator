8f96ce076e1f0fc3c0bd61fa722d83a6f2ba8d53
Bruno Vieira <76965972+BrunoVieira003@users.noreply.github.com>
2021-11-11 17:03:58 -0300

    Adiciona geração de template básico de datapack
    Através da execução do arquivo main.py, é possível gerar por
    linha de comando um template básico de datapack

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from pack_builder import Pack
 
 db_manager = DBManager("data/data.db")
 
+version_translation = {"1.16": 6, "1.15": 5, "1.14": 4}
 
 name = input("Datapack name: ")
 namespace = input("Datapack namespace: ")
 desc = input("Datapack description(optional): ")
 version = input("Datapack version: ")
 
-db_manager.insert(name, namespace, desc, version)
+db_manager.insert(name, namespace, desc, version_translation[version])
 
 if input("Generate?[y/n]: ").lower() == "y":
     pack = Pack(db_manager.get_pack(name))
--- pack_builder.py
+++ pack_builder.py
@@ -2,8 +2,6 @@ import json
 import os
 
 class Pack:
-    _version_translation = {"1.16": 6, "1.15": 5, "1.14": 4}
-
     def __init__(self, pack_data):
         self.name = pack_data[0]
         self.namespace = pack_data[1]
@@ -11,10 +9,31 @@ class Pack:
         self.version = pack_data[3]
     
     def generate(self, path):
+        os.makedirs(f"{path}/{self.name}/data/minecraft/tags/functions")
+
         pack_path = f"{path}/{self.name}/data/{self.namespace}"
         os.makedirs(f"{pack_path}/functions")
+        os.mkdir(f"{pack_path}/tags")
+        os.mkdir(f"{pack_path}/tags/blocks")
+        os.mkdir(f"{pack_path}/tags/items")
+        os.mkdir(f"{pack_path}/tags/entity_types")
+        os.mkdir(f"{pack_path}/advancements")
+        os.mkdir(f"{pack_path}/predicates")
+        os.mkdir(f"{pack_path}/loot_tables")
+        os.mkdir(f"{pack_path}/recipes")
 
-        pack_format = self._version_translation.get(self.version) if self._version_translation.get(self.version) != None else 0
-        pack_meta = {"pack":{"pack_format": pack_format, "description":self.desc}}
+        pack_meta = {"pack":{"pack_format": self.version, "description":self.desc}}
         with open(f"{path}/{self.name}/pack.mcmeta", "w") as arc:
             arc.write(json.dumps(pack_meta, indent=1))
+        
+        # generate and config of tick and load functions
+        load_config = {"values": [f"{self.namespace}:init"]}
+        tick_config = {"values": [f"{self.namespace}:loop"]}
+        with open(f"{path}/{self.name}/data/minecraft/tags/functions/load.json", "w") as load:
+            load.write(json.dumps(load_config, indent=1))
+        with open(f"{path}/{self.name}/data/minecraft/tags/functions/tick.json", "w") as tick:
+            tick.write(json.dumps(tick_config, indent=1))
+        
+        for func in ["init", "loop"]:
+            with open(f"{pack_path}/functions/{func}.mcfunction", "w") as arc:
+                pass
